c8d96e0ca6ecf8a54878d15deff5c4b318cdb82c
Nadjem-47 <nadgmo@gmail.com>
2025-11-04 19:28:35 +0100

    Example two

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,47 +1,12 @@
-import { useEffect } from 'react';
-import { View } from 'react-native';
-import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSpring, withTiming } from 'react-native-reanimated';
-
-const size = 100;
-
-
-
-const handleRotation = (progress) => {
-  "worklet"
-  return `${progress.value * 2 * Math.PI}rad` 
-}
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+import Home from "./home"
 
 export default function RootLayout() {
-
-  const progress = useSharedValue(1);
-  const scale = useSharedValue(1)
-
-   
-
-
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: progress.value,
-      transform: [{ scale: scale.value }, { rotate: handleRotation(progress) }],
-    };
-  });
-
-  
-
-
-  useEffect(() => {
-    progress.value = withRepeat(withTiming(1, {duration: 1000}), -1, true);
-    scale.value = withRepeat(withSpring(3), -1, true)
-
-  }, [])
-  
-
-console.log(reanimatedStyle);
-
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Animated.View style={[{width: size, height: size, backgroundColor: 'red'}, reanimatedStyle]} />
-    </View>
-  );
+    <GestureHandlerRootView>
+      <Home />
+    </GestureHandlerRootView>
+  )
 }
- 
+
+
